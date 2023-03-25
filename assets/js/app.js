07114c0b97fe2722935a97fa5b0690d3070dc894
gvillablanca <g.villalobo.m@gmail.com>
2023-03-25 02:13:45 -0300

    login operativo

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,24 +1,10 @@
-$('.ir-arriba').click(function(){
-    $('body, html').animate({
-      scrollTop: '0px'
-    }, 700);
-});
-
-$(window).scroll(function(){
-    if( $(this).scrollTop() > 0 ){
-      $('.ir-arriba').slideDown(300);
-    } else {
-      $('.ir-arriba').slideUp(300);
-    }
-}); 
-
 function logear(){
     var username = document.getElementById("username").value;
     var password = document.getElementById("password").value;
 
     if (username != "" && password != "" && username !="admin") {
         document.getElementById('login-btn').style.display = 'none';
-        document.getElementById('user-btn').style.display = 'block';
+        document.getElementById('user-btn').style.display = 'block';        
     }
     else if(username != "" && password != "" && username =="admin"){
         window.location.href = "../ejercicioPracticoHtml/index.html";
